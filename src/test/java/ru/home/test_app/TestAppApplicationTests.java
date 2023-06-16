c22e946520cbd6caf6683ff08393bbaebf835edd
Daniil Epikhov <depikhov29@gmail.com>
2023-06-16 22:22:40 +0300

    init commit

---
+++ src/test/java/ru/home/test_app/TestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.test_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
